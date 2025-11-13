56a40788f0999ac981378fafff4b52c1405d2a3a
bezael <bezael@gmail.com>
2025-11-13 19:47:31 +0100

    install and setup  jest

---
+++ candidate/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  testEnvironment: 'jsdom',
+};
+
+export default config;
