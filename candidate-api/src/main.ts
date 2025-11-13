901718f653e415c41da4f75267e5a9d19cb674a9
bezael <bezael@gmail.com>
2025-11-13 20:04:44 +0100

    feat: :sparkles: enable CORS, set global prefix, and add validation pipes in main.ts

--- candidate-api/src/main.ts
+++ candidate-api/src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
