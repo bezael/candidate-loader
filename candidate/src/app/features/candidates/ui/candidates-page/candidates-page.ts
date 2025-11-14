c976f4c023d0e23865e694f56f1bec796bb570f7
bezael <bezael@gmail.com>
2025-11-14 11:30:49 +0100

    feat: :sparkles: enhance candidates page

--- candidate/src/app/features/candidates/ui/candidates-page/candidates-page.ts
+++ candidate/src/app/features/candidates/ui/candidates-page/candidates-page.ts
@@ -1,14 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { CandidateResponse } from '@app/features/candidates/data/models/candidate.model';
 import { CandidateForm } from '@app/features/candidates/ui/candidates-form/candidate-form';
 
 @Component({
   selector: 'app-candidates-page',
-  imports: [CandidateForm],
-  template: ` 
-  <div class="candidates-page">
-    <app-candidate-form  />
+  imports: [CandidateForm, JsonPipe],
+  template: ` <div class="candidates-page">
+    <app-candidate-form (candidateUploaded)="onCandidateUploaded($event)" />
+    <pre>{{ allCandidates() | json }}</pre>
   </div>`,
   styleUrl: './candidates-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CandidatesPage {}
+export class CandidatesPage {
+  private readonly _candidate = signal<CandidateResponse[]>([]);
+
+  public readonly allCandidates = computed<CandidateResponse[]>(() => {
+    const newOne = this._candidate();
+    return [...newOne];
+  });
+
+  public onCandidateUploaded(candidate: CandidateResponse): void {
+    this._candidate.update((current: CandidateResponse[]) => [...current, candidate]);
+  }
+}
