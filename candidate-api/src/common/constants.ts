46693ea53ae623a051392058ef9d38378bdb0c8e
bezael <bezael@gmail.com>
2025-11-17 11:07:58 +0100

    feat: :sparkles: add configuration constants

---
+++ candidate-api/src/common/constants.ts
@@ -0,0 +1,14 @@
+export const SERVER_CONFIG = {
+  DEFAULT_PORT: 3000,
+} as const;
+
+export const FILE_CONFIG = {
+  MAX_FILE_SIZE_BYTES: 5 * 1024 * 1024,
+  MAX_FILE_SIZE_MB: 5,
+} as const;
+
+export const EXCEL_CONFIG = {
+  FIRST_SHEET_INDEX: 0,
+  FIRST_ROW_INDEX: 0,
+} as const;
+
